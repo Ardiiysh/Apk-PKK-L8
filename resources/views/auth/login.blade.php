f3de2854fdc6403a9f195dc5569da4fabdea6e1c
ardiiysh <ardiyansah4316@gmail.com>
2022-10-01 01:06:51 +0700

    hapus login with google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                                         <body class="hold-transition login-page">
                                         <div class="login-box">
                                         <div class="login-logo">
-                                            <a href="{{asset('template')}}/index2.html"><b>Admin</b>LTE</a>
+                                            <a href="{{asset('template')}}/index2.html"><b>PKK</b>Digital</a>
                                         </div>
                                         <!-- /.login-logo -->
                                         <div class="card">
@@ -77,7 +77,7 @@
                                                 </div>
                                             </form>
 
-                                            <div class="social-auth-links text-center mb-3">
+                                            {{-- <div class="social-auth-links text-center mb-3">
                                                 <p>- OR -</p>
                                                 <a href="#" class="btn btn-block btn-primary">
                                                 <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
@@ -85,7 +85,7 @@
                                                 <a href="#" class="btn btn-block btn-danger">
                                                 <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
                                                 </a>
-                                            </div>
+                                            </div> --}}
                                             <!-- /.social-auth-links -->
 
                                             <p class="mb-1">
