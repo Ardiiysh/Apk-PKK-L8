65048f5ee8ebaeb20c43c5af34a37aa3d5a85885
GoPieRRe <98568059+GoPieRRe@users.noreply.github.com>
2022-12-02 22:46:37 +0700

    revisi user page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,13 +27,6 @@ class UserController extends Controller
                         $btn = '
                         <form onsubmit="return confirm(\'Apakah anda yakin ingin menghapus '.$row->name.' ?\');"  action="dataUser/'.$row->id.'" method="POST">
 
-                            <a class="btn btn-primary" href="dataUser/'.$row->id.'/edit" >
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                            </svg>
-                            </a>
-
                             '.csrf_field().'
                             '.method_field("DELETE").'
 
@@ -139,11 +132,17 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy(User $User)
+    public function destroy(User $dataUser)
     {
-        $User->delete();
+        $cek =$dataUser->delete();
+
+        if ($cek) {
+            return redirect()->back()->with('success', 'Deleted user Successfully');
+        }else{
+            return redirect()->back()->with('error', 'Deleted user Unsuccessfully');
+        }
     }
 }
