9110ad1a6f5d6b62207ec78ec746fa4ed745d21e
ardiiysh <ardiyansah4316@gmail.com>
2022-12-03 18:01:20 +0700

    halaman dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,71 +1,60 @@
 @extends('layouts.layout')
  @section('title','Dashboard')
-
+ 
 @section('content')
+
+
+
  <!-- Main content -->
  <section class="content">
    <!-- Main content -->
    <section class="content">
     <div class="container-fluid">
-      <h1>Selamat Datang {{ Auth::User()->name }} !</h1>
 
       <!-- Info boxes -->
       <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box">
-            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Demo</span>
-              <span class="info-box-number">
-                10
-                <small>%</small>
-              </span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
+        <div class="col-12 col-sm-6 col-md-6">
+          <div >
+            
+        <h5 class="text-center" > Selamat Datang di Aplikasi </h5>
+        <div class="text-info">
+          <h4 class="text-center" >SMART PKK (NAMA DESA)</h4>
         </div>
-        <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
-            <div class="info-box-content">
-              <span class="info-box-text">Demo Likes</span>
-              <span class="info-box-number">41,410</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
+    </div>  
+        <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-
+ 
         <!-- fix for small devices only -->
         <div class="clearfix hidden-md-up"></div>
-
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Demo Sales</span>
-              <span class="info-box-number">760</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
+ 
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
-          <div class="info-box mb-3">
-            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Demo Users</span>
-              <span class="info-box-number">2,000</span>
+        <div class="col-12 col-sm-6 col-md-6">
+          <div class="img-fluid"    alt="Responsive image" >
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="{{ asset('img/pkk.png') }}" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="{{ asset('img/ponsel.png') }}" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="{{ asset('img/10pokok.png') }}" alt="Third slide">
+              </div>
             </div>
-            <!-- /.info-box-content -->
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+            
+            </div>
+              <!-- /.info-box-content -->
           </div>
           <!-- /.info-box -->
         </div>
