75f1ede37a69a3aad8c3c6a0bc28790b28cf5d91
GoPieRRe <98568059+GoPieRRe@users.noreply.github.com>
2023-02-02 14:38:00 +0700

    layout kop surat pdf

--- resources/views/bukuPerpustakaans/laporan_pdf.blade.php
+++ resources/views/bukuPerpustakaans/laporan_pdf.blade.php
@@ -40,13 +40,7 @@
         <img src="kopsurat.png" alt="">
         <h1>INI PDF PKK CIHUYYYY</h1>
     </div> -->
-    <table width="100%">
-        <tr>
-            <td width="25" align="center"><img src="{{ public_path('img/logopkk.png') }}" width="100%"></td>
-            <td width="50" align="center"><h1>Pemberdayaan dan kesejahteraan Keluarga</h1><h1>(PKK)</h1><h2>Desa pasawahan</h2><h5>Alamat:Jalan Otto Iskandardinata kampung tanjung RT 003/RW 013, Pasawahan, Kec. Tarogong Kaler, Kabupaten Garut, Jawa Barat 44151</h5></td>
-            <td width="25" align="center"><img src="{{ public_path('img/logopkk.png') }}" width="100%"></td>
-        </tr>
-        </table>
+    @include('layouts.kop_surat')
         <hr><width="100" height="75"></hr>
     <table class="table1" style="margin-left: auto; margin-right:auto; margin-top: 30px;" border="1">
         <thead>
